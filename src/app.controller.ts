d553ac774f3c0160f876c1ed47eb51c69114bb88
r8bywork <vanya.smolensky@gmail.com>
2023-10-20 17:45:12 +0300

    db setup, add docker, add prisma, change prisma model, add auth setup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOkResponse, ApiProperty } from '@nestjs/swagger';
+
 import { AppService } from './app.service';
+import { DbService } from './db/db.service';
+class HelloWorldDto {
+  @ApiProperty()
+  message: string;
+}
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private DbService: DbService,
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @ApiOkResponse({
+    type: HelloWorldDto,
+  })
+  async getHello(): Promise<HelloWorldDto> {
+    const users = await this.DbService.user.findMany({});
+    console.log(users);
+    return { message: this.appService.getHello() };
   }
 }
