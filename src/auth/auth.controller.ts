fe6f37cdf4d25fa201e665757d58012721f8bdf5
r8bywork <vanya.smolensky@gmail.com>
2023-10-20 19:34:32 +0300

    add sign-up, records in cookies, jwt token, jwt parser, cookieService

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,55 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Res,
+} from '@nestjs/common';
+import { ApiCreatedResponse, ApiOkResponse } from '@nestjs/swagger';
+import { Response } from 'express';
+import { AuthService } from './auth.service';
+import { CookieService } from './cookie.service';
+import { GetSessionInfoDto, SignInBodyDto, SignUpBodyDto } from './dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(
+    private authService: AuthService,
+    private cookieService: CookieService,
+  ) {}
+
+  @Post('sign-up')
+  @ApiCreatedResponse()
+  async signUp(
+    @Body() body: SignUpBodyDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const { accessToken } = await this.authService.signUp(
+      body.email,
+      body.password,
+    );
+    this.cookieService.setToken(res, accessToken);
+  }
+
+  @Post('sign-in')
+  @ApiOkResponse()
+  @HttpCode(HttpStatus.OK)
+  signIn(@Body() body: SignInBodyDto) {
+    return null;
+  }
+
+  @Post('sign-out')
+  @ApiOkResponse()
+  @HttpCode(HttpStatus.OK)
+  signOut() {}
+
+  @Get('session')
+  @ApiOkResponse({
+    type: GetSessionInfoDto,
+  })
+  getSessionInfo() {
+    return null;
+  }
+}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { PasswordService } from './password.service';
 import { CookieService } from './cookie.service';
+import { PasswordService } from './password.service';
 
 @Module({
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService, PasswordService, CookieService]
+  providers: [AuthService, PasswordService, CookieService],
 })
 export class AuthModule {}
